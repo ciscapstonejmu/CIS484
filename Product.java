02467bb4d0edfdb8fae06bdf112f696eea313b55
ciscapstonejmu <50637362+ciscapstonejmu@users.noreply.github.com>
2019-06-03 19:58:56 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -3,6 +3,7 @@ package pkg484groupproj;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 
@@ -14,7 +15,10 @@ public class Product {
     private String location;
     private String foodDescription;
     private String category;
+    private int aisle;
     private BufferedImage image;
+    ArrayList<Double> availableDiscounts = new ArrayList<>();
+    ArrayList<String> discountTitle = new ArrayList<>();
     public static int nextID = 0;
     
     
@@ -27,6 +31,47 @@ public class Product {
         this.foodDescription = foodDescription;
         this.category = category;
         this.image = image;
+        
+        if (category.equalsIgnoreCase("PRODUCE"))
+        {
+            this.aisle = 1;
+        }
+        
+        if (category.equalsIgnoreCase("DAIRY"))
+        {
+            this.aisle = 2;
+        }
+        
+        if (category.equalsIgnoreCase("CANDY"))
+        {
+            this.aisle = 3;
+        }
+        
+        
+        if (category.equalsIgnoreCase("CANNED FOODS"))
+        {
+            this.aisle = 4;
+        }
+        
+        if (category.equalsIgnoreCase("PASTA"))
+        {
+            this.aisle = 5;
+        }
+        
+        if (category.equalsIgnoreCase("SNACK FOODS"))
+        {
+            this.aisle = 6;
+        }
+        
+        if (category.equalsIgnoreCase("BAKED GOODS"))
+        {
+            this.aisle = 7;
+        }
+        
+        if (category.equalsIgnoreCase("FROZEN FOODS"))
+        {
+            this.aisle = 8;
+        }
     }
     
     public int getID()
@@ -94,6 +139,16 @@ public class Product {
         return this.category;
     }
     
+    public void setAisle(int aisle)
+    {
+        this.aisle = aisle;
+    }
+    
+    public int getAisle()
+    {
+        return this.aisle;
+    }
+    
     public void setImage (BufferedImage image)
     {
         this.image = image;
@@ -102,6 +157,17 @@ public class Product {
     public BufferedImage getImage()
     {
         return this.image;
+    }
+    
+    public void addDiscount(String t, double d)
+    {
+        this.availableDiscounts.add(d);
+        this.discountTitle.add(t);
+    }
+    
+    public void applyDiscount(int index)
+    {
+        this.price = this.price - this.availableDiscounts.get(index);
     }
     
     public String toString()
