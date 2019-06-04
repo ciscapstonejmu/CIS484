97e32011a0b5c68900f3f6356e41c159c2bb3df1
ciscapstonejmu <50637362+ciscapstonejmu@users.noreply.github.com>
2019-06-04 16:45:35 -0400

    Update CustSaleForm.java

--- CustSaleForm.java
+++ CustSaleForm.java
@@ -15,13 +15,14 @@ public class CustSaleForm {
     public String saleID;
     public static int saleCount = 0;
     
+    public Label lblSaleDate = new Label("Date:");
     public Label lblSaleStore = new Label("Store:");
     public Label lblSaleCust = new Label("Customer:");
     public Label lblClubMem = new Label("Thrifty Club Member?");
     public Label lblSaleProd = new Label("Product:");
-    public Label lblSaleProdQuan = new Label("Quantity:");
+    //public Label lblSaleProdQuan = new Label("Quantity:");
     
-    public TextField txtSaleProdQuan = new TextField();
+    //public TextField txtSaleProdQuan = new TextField();
     
     public Button btnAddToSale = new Button("Add to Sale ->");
     public Button btnCompleteSale = new Button("Complete Sale ->");
@@ -35,6 +36,10 @@ public class CustSaleForm {
             );
     final ComboBox cmboIsMem = new ComboBox(isMemList);
     
+    public DatePicker saleDate = new DatePicker();
+    
+    public ArrayList<Product> saleProducts = new ArrayList();
+    
     public GUI myParent;
     
     //Sale Constructor
@@ -47,21 +52,25 @@ public class CustSaleForm {
         primaryPane.setAlignment(Pos.CENTER);
         
         //Set Controls
-        primaryPane.add(lblSaleStore, 0, 0);
-        primaryPane.add(myParent.cmboStore, 1, 0);
-        primaryPane.add(lblSaleCust, 0, 1);
-        primaryPane.add(myParent.cmboCust, 1, 1);
-        primaryPane.add(lblClubMem, 0, 2);
-        primaryPane.add(cmboIsMem, 1, 2);
-        primaryPane.add(lblSaleProd, 0, 3);
-        primaryPane.add(myParent.cmboProd, 1, 3);
-        primaryPane.add(lblSaleProdQuan, 0, 4);
-        primaryPane.add(txtSaleProdQuan, 1, 4);
+        
+        primaryPane.add(lblSaleDate, 0, 0);
+        primaryPane.add(saleDate, 1, 0);
+        primaryPane.add(lblSaleStore, 0, 1);
+        primaryPane.add(myParent.cmboStore, 1, 1);
+        primaryPane.add(lblSaleCust, 0, 2);
+        primaryPane.add(myParent.cmboCust, 1, 2);
+        primaryPane.add(lblClubMem, 0, 3);
+        primaryPane.add(cmboIsMem, 1, 3);
+        primaryPane.add(lblSaleProd, 0, 4);
+        primaryPane.add(myParent.cmboProd, 1, 4);
+        //primaryPane.add(lblSaleProdQuan, 0, 5);
+        //primaryPane.add(txtSaleProdQuan, 1, 5);
         primaryPane.add(btnAddToSale, 0, 5, 2, 2);
         primaryPane.add(btnCompleteSale, 0, 6, 2, 2);
         primaryPane.add(txtSaleOutput, 2, 0, 4, 6);
         
-        txtSaleProdQuan.setPrefWidth(20);
+        //txtSaleProdQuan.setPrefWidth(20);
+        saleDate.setPrefWidth(100);
         
         Scene primaryScene = new Scene(primaryPane, 450, 450);
         
