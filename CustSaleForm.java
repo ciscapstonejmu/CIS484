c73a66493fd69d0d25e4211845783fcf012e45c7
ciscapstonejmu <50637362+ciscapstonejmu@users.noreply.github.com>
2019-06-04 17:46:57 -0400

    Update CustSaleForm.java

--- CustSaleForm.java
+++ CustSaleForm.java
@@ -59,14 +59,18 @@ public class CustSaleForm {
         
         primaryPane.add(lblSaleDate, 0, 0);
         primaryPane.add(saleDate, 1, 0);
+        saleDate.setMinWidth(3);
         primaryPane.add(lblSaleStore, 0, 1);
-        primaryPane.add(cmboSaleStore, 1, 1);
+        primaryPane.add(myParent.cmboStore, 1, 1);
         primaryPane.add(lblSaleCust, 0, 2);
-        primaryPane.add(cmboSaleCust, 1, 2);
+        primaryPane.add(myParent.cmboCust, 1, 2);
+        myParent.cmboCust.setMinWidth(3);
         primaryPane.add(lblClubMem, 0, 3);
         primaryPane.add(cmboIsMem, 1, 3);
+        cmboIsMem.setMinWidth(3);
         primaryPane.add(lblSaleProd, 0, 4);
-        primaryPane.add(cmboSaleProd, 1, 4);
+        primaryPane.add(myParent.cmboProd, 1, 4);
+        myParent.cmboProd.setMinWidth(3);
         //primaryPane.add(lblSaleProdQuan, 0, 5);
         //primaryPane.add(txtSaleProdQuan, 1, 5);
         primaryPane.add(btnAddToSale, 0, 5, 2, 2);
@@ -75,9 +79,8 @@ public class CustSaleForm {
         
         //txtSaleProdQuan.setPrefWidth(20);
         saleDate.setPrefWidth(100);
-        txtSaleOutput.setEditable(false);
         
-        Scene primaryScene = new Scene(primaryPane, 450, 450);
+        Scene primaryScene = new Scene(primaryPane, 800, 600);
         
         Stage primaryStage = new Stage();
         primaryStage.setScene(primaryScene);
@@ -97,6 +100,14 @@ public class CustSaleForm {
             
         });
         
+        //Complete the Sale
+        btnCompleteSale.setOnAction(e -> {
+        primaryPane.add(receiptOutput, 5, 0);
+        receiptOutput.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            for (CustSale saleList : GUI.saleList) {
+                receiptOutput.appendText(saleList.toString());
+            }
+            });
         
     }
     
