51656cbcc7d70cac76f7d03c28dda2b91332a3e2
ciscapstonejmu <50637362+ciscapstonejmu@users.noreply.github.com>
2019-06-11 15:42:55 -0400

    Update Expense.java

--- Expense.java
+++ Expense.java
@@ -2,14 +2,22 @@ package pkg484groupproj;
 
 public class Expense {
     private int expenseID;
-    private String expenseType;
+    private String category;
+    private String description;
+    private String date;
+    private String status;
     private double amountPaid;
+    
     public static int nextID = 0;
     
-    public Expense(String expenseType, double amountPaid)
+    public Expense(String category, String description, double amountPaid, String date, String status)
     {
-        this.expenseType = expenseType;
+        this.category = category;
+        this.description = description;
         this.amountPaid = amountPaid;
+        this.date = date;
+        this.status = status;
+        
         expenseID = nextID++;
     }
     
@@ -18,14 +26,24 @@ public class Expense {
         return this.expenseID;
     }
     
-    public void setType (String expenseType)
+    public void setCategory (String expenseType)
     {
-        this.expenseType = expenseType;
+        this.category = expenseType;
     }
     
-    public String getType()
+    public String getCategory()
     {
-        return this.expenseType;
+        return this.category;
+    }
+    
+    public void setDescription(String desc)
+    {
+        this.description = desc;
+    }
+    
+    public String getDescription()
+    {
+        return this.description;
     }
     
     public void setAmountPaid(double amountPaid)
@@ -38,10 +56,22 @@ public class Expense {
         return this.amountPaid;
     }
     
+    public String getDate()
+    {
+        return this.date;
+    }
+    
+    public String getStatus()
+    {
+        return this.status;
+    }
+    
     public String toString()
     {
         String str = "";
-        str += "ID: " + this.expenseID + " , Amount Paid: " + this.amountPaid;
+        str += "ID: " + this.expenseID + "Description: " + this.description + 
+                " Category: " + this.category + " Amount Paid: " + this.amountPaid +
+                " Date: " + this.date + " Status: " + this.status;
         return str;
     }
     
