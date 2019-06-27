6fed0d7fb5e40a8b096e9a71fd81daa4faefd2e0
ciscapstonejmu <50637362+ciscapstonejmu@users.noreply.github.com>
2019-06-26 20:16:02 -0400

    Update Product.java

--- Product.java
+++ Product.java
@@ -123,8 +123,7 @@ public class Product implements Serializable{
         this.foodDescription = foodDescription;
         this.category = category;
         this.supp = supp; 
-        this.supplier = supp.getName();
-        this.image = image;        
+        this.supplier = supp.getName();       
         this.productID = nextID++;
         this.store = store; 
         this.cost = cost;
@@ -329,21 +328,5 @@ public class Product implements Serializable{
         str += "ID: " + this.productID + " " + this.productName +  
                 " , Price: " + formatter.format(this.price);
         return str;
-    }
-    public static void writeObject(ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject();
-        out.writeInt(images.size()); // how many images are serialized?
-        for (BufferedImage eachImage : images) {
-            ImageIO.write(eachImage, "png", out); // png is lossless
-        }
-    }
-
-    public static void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        final int imageCount = in.readInt();
-        images = new ArrayList<BufferedImage>(imageCount);
-        for (int i=0; i<imageCount; i++) {
-            images.add(ImageIO.read(in));
-        }
     }
 }
